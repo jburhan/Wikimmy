93e002750e1d0e9d4e5d153a9dc39bf6286d1ffc
jburhan <jimburhan@gmail.com>
2018-04-19 06:52:37 +0700

    CRUD final revision

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,6 @@ class PagesController < ApplicationController
     @page = Page.new
     @page.title = params[:page][:title]
     @page.body = params[:page][:body]
-    @wiki.private = params[:wiki][:private]
     @page.user = current_user
 
     if @page.save
@@ -36,7 +35,6 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     @page.title = params[:page][:title]
     @page.body = params[:page][:body]
-    @wiki.private = params[:wiki][:private]
     @page.user = current_user
 
     if @page.save
